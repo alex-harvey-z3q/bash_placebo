e4af3b4d4b09ea97b09110cb4e7dbd3edd5c0208
Alex Harvey <alexharv074@gmail.com>
2018-09-17 19:53:30 +1000

    First refactor

--- placebo.sh
+++ placebo.sh
@@ -1,53 +1,71 @@
-if [ -z $DATA_PATH ]; then
-  echo "Expected variable DATA_PATH to be set"
+if ! echo $PILL | grep -q "(playback|record)" ; then
+  echo "PILL must be set to playback or record"
+  return 1
+fi
+
+if [ -z $DATA_PATH ] ; then
+  echo "DATA_PATH must be set"
   return 1
 fi
 
 mkdir -p $DATA_PATH
 
+create_new() {
+  local f=$1
+  echo 'case "aws $*" in' > $f
+}
+
+update_existing() {
+  local f=$1
+  awk '/^esac/{exit}{print}' $f > $f.bak
+  mv $f.bak $f
+}
+
 record() {
   local f=$1
   local com=$2
   local sub=$3
-
   shift ; shift ; shift
 
-  if [ -e $f ]; then
-    awk '/esac/{exit}{print}' $f > $f.bak
-    mv $f.bak $f
+  if [ -e $f ] ; then
+    update_existing $f
   else
-    echo 'case "aws $*" in' > $f
+    create_new $f
   fi
 
-  echo '"aws $*")'     >> $f
-  echo "  cat <<'EOF'" >> $f
+  cat >> $f <<'EOD'
+"aws $*")
+  cat <<'EOF'
+EOD
 
-  command aws $com $sub $* | tee -a $f
+  command aws $com $sub $* | \
+    tee -a $f
 
-  echo 'EOF'           >> $f
-  echo '  ;;'          >> $f
-  echo 'esac'          >> $f
+  cat >> $f <<'EOD'
+EOF
+  ;;
+esac
+EOD
 }
 
 aws() {
-  com=$1 ; sub=$2 ; shift ; shift
+  local com=$1
+  local sub=$2
+  shift ; shift
 
   var_name=$(echo "$com$sub" | sed -e 's/-//g')
-  eval "(( ${var_name}++ ))"
-  eval "local c=\$${var_name}"
 
-  data_file_name=$DATA_PATH/${com}.${sub}_${c}
+  eval "(( ${var_name}++ ))"
+  eval 'local counter=$'"$var_name"
+
+  data_file=${DATA_PATH}/${com}.${sub}.${counter}
 
   case "$PILL" in
-    "playback")
-      source $data_file_name $com $sub $*
-      ;;
-    "record")
-      record $data_file_name $com $sub $*
-      ;;
-    *)
-      echo "PILL must be set to playback or \
-record but is set to '$PILL'"
-      ;;
+  "playback")
+    source $data_file $com $sub $*
+    ;;
+  "record")
+    record $data_file $com $sub $*
+    ;;
   esac
 }
